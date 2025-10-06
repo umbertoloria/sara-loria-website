6f0b2546b3a87db6a66ab74ce416923446800fdd
Umberto Loria <umberto.loria@gmail.com>
2025-10-06 13:24:34 +0200

    Page "Tracce di un passato" alt texts added

--- src/app/tracce-passato/page.tsx
+++ src/app/tracce-passato/page.tsx
@@ -19,26 +19,61 @@ export default function TraccePassatoPageContent() {
           the gaze to slow, listen, and discover echoes of a past still alive.
         </p>
         <div className='line' />
-        {/* TODO: Put "alt"s here */}
         <GridReal>
-          <img src='/tracce-passato/0.jpg' alt='' />
-          <img src='/tracce-passato/1.jpg' alt='' />
-          <img src='/tracce-passato/2.jpg' alt='' />
-          <img src='/tracce-passato/3.jpg' alt='' />
-          <img src='/tracce-passato/4.jpg' alt='' />
-          <img src='/tracce-passato/5.jpg' alt='' />
-          <img src='/tracce-passato/6.jpg' alt='' />
-          <img src='/tracce-passato/7.jpg' alt='' />
-          <img src='/tracce-passato/8.jpg' alt='' />
-          <img src='/tracce-passato/9.jpg' alt='' />
-          <img src='/tracce-passato/10.jpg' alt='' />
-          <img src='/tracce-passato/11.jpg' alt='' />
-          <img src='/tracce-passato/12.jpg' alt='' />
-          <img src='/tracce-passato/13.jpg' alt='' />
-          <img src='/tracce-passato/14.jpg' alt='' />
-          <img src='/tracce-passato/15.jpg' alt='' />
-          <img src='/tracce-passato/16.jpg' alt='' />
-          <img src='/tracce-passato/17.jpg' alt='' />
+          <img src='/tracce-passato/0.jpg' alt='Window with grid' />
+          <img
+            src='/tracce-passato/1.jpg'
+            alt='Mosaic representing Baronissi'
+          />
+          <img src='/tracce-passato/2.jpg' alt='Ancient walled-up arch' />
+          <img
+            src='/tracce-passato/3.jpg'
+            alt='New fresco next to an old arch'
+          />
+          <img src='/tracce-passato/4.jpg' alt='Door no longer accessible' />
+          <img
+            src='/tracce-passato/5.jpg'
+            alt='Decaying wall next to a wall under construction'
+          />
+          <img src='/tracce-passato/6.jpg' alt='Ancient unknown tool' />
+          <img
+            src='/tracce-passato/7.jpg'
+            alt='Old one-story building renovated'
+          />
+          <img
+            src='/tracce-passato/8.jpg'
+            alt='Ancient wall next to a newly built wall'
+          />
+          <img src='/tracce-passato/9.jpg' alt='Old walled-up door' />
+          <img
+            src='/tracce-passato/10.jpg'
+            alt='Grass growing spontaneoulsy on the walls of a house'
+          />
+          <img
+            src='/tracce-passato/11.jpg'
+            alt='Remains of a Roman villa imprisoned'
+          />
+          <img
+            src='/tracce-passato/12.jpg'
+            alt='Recently built bench leaning against a decayed wall'
+          />
+          <img
+            src='/tracce-passato/13.jpg'
+            alt='Decayed building, except for one balcony that is completely new and renovated'
+          />
+          <img src='/tracce-passato/14.jpg' alt='Ancient walled-up doorway' />
+          <img
+            src='/tracce-passato/15.jpg'
+            alt='Remains of an old wall and small wall in constract with the new houses'
+          />
+          <img
+            src='/tracce-passato/16.jpg'
+            alt='Closed gallery with rough equipment'
+          />
+          <img
+            src='/tracce-passato/17.jpg'
+            alt='Ancient doorway next to a new wall'
+          />
         </GridReal>
       </section>
     </div>
