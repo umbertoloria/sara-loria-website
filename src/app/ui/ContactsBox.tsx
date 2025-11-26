fd911e2fbf8798b7cebd9041b3cb7d67bccfc4c0
Umberto Loria <umberto.loria@gmail.com>
2025-11-26 18:04:08 +0100

    Contacts Box with items horizontally aligned

--- src/app/ui/ContactsBox.tsx
+++ src/app/ui/ContactsBox.tsx
@@ -6,35 +6,45 @@ export const ContactsBox: FC = () => {
 			<div className='layout'>
 				<div className='contacts-box'>
 					<div className='linkedin'>
-						<span>
-							<img src='/social/linkedin.png' alt='LinkedIn' />
-						</span>
+						<img src='/social/linkedin.png' alt='LinkedIn' />
 						<a
 							href='https://www.linkedin.com/in/sara-loria-b680b3305'
 							target='_blank'
+							style={{
+								paddingTop: 8,
+							}}
 						>
 							Sara Loria
 						</a>
 					</div>
 
 					<div className='instagram'>
-						<span>
-							<img src='/social/instagram.png' alt='Instagram' />
-						</span>
-						<a href='https://www.instagram.com/sl.oreea' target='_blank'>
+						<img src='/social/instagram.png' alt='Instagram' />
+						<a
+							href='https://www.instagram.com/sl.oreea'
+							target='_blank'
+							style={{
+								paddingTop: 5,
+							}}
+						>
 							@sl.oreea
 						</a>
 					</div>
 
 					<div className='email'>
-						<span>
-							<img src='/social/email.png' alt='Email' />
-						</span>
-						<a href='mailto:saraloria02@gmail.com'>saraloria02@gmail.com</a>
+						<img src='/social/email.png' alt='Email' />
+						<a
+							href='mailto:saraloria02@gmail.com'
+							style={{
+								paddingTop: 2,
+							}}
+						>
+							saraloria02@gmail.com
+						</a>
 					</div>
-
-					<div className='location'>Based in Rome, Italy</div>
 				</div>
+
+				<div className='location'>Based in Rome, Italy</div>
 			</div>
 		</footer>
 	)
